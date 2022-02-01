7282f197ebbe01f4be89c88571a537cb1f23399f
John <johnmulvey48@yahoo.com>
2022-02-01 10:46:23 -0500

    git@github.com:Jmulvez/phase-1-algorithms-has-target-sum.git

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for(let i = 0; i < array.length; i++) {
+    const complement = target - array[i]
+    for(let j = i + 1; j < array.length; j++) {
+      if(array[j] === complement) return true
+    }
+  }
 }
-
+return false
 /* 
-  Write the Big O time complexity of your function here
+  Runtime: O(n^2)
+  Space: O(n)
 */
 
 /* 
-  Add your pseudocode here
+  iterate through the array 
+    for the first number, find a complementary number that adds to the target number (complement = target - number)
+    iterate through the rest of the array
+      check if any of the numbers added to the firs number add up to the target number
+      if the two numbers add up to the target, return true
+    if no two numbers add up to the target number, return false
 */
 
 /*
-  Add written explanation of your solution here
+  Write a function that iterates over an array and returns true if the sum of two of 
+  those numbers adds up to the target number.
 */
 
 // You can run `node index.js` to view these console logs
